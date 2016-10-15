ba0c81e65e2d1479b303e786314e34fb5816f37f
c6y <k@eboy.com>
2016-10-15 02:40:38 -0700

    modularized pixInput

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,12 +5,14 @@ import { EboyPix } from '../api/eboypix/eboypix.js';
 import { Colors } from '../api/colors/colors.js';
 
 import './body.html';
-import './components/pic.js';
 import './components/pic.html';
-import './components/color.js';
+import './components/pic.js';
 import './components/color.html';
+import './components/color.js';
 import './components/colorInput.html';
 import './components/colorInput.js';
+import './components/pixInput.html';
+import './components/pixInput.js';
 
 Template.body.helpers({
   pix() {
@@ -28,37 +30,6 @@ Template.body.helpers({
 });
 
 Template.body.events({
-  'submit .new-pic-batch'(event) {
-    event.preventDefault();
-    // Assign form input to constant
-    const target = event.target;
-    const imgBatchURLs = target.batchchurls.value;
-    // Remove line breaks
-    const imgBatchURLsClean = imgBatchURLs.replace(/[\r\n]/g, ',');
-    console.log('imgBatchURLsClean: ' + imgBatchURLsClean);
-
-    // Create an array and populate with urls
-    let urls = [];
-    urls = imgBatchURLsClean.split(",");
-
-    // Only keep elements that start with 'http'
-    urls = urls.filter(function(element) {
-      return element.startsWith('http');
-    });
-
-    (function nextImage(urls) {
-      if (urls.length) {
-        let img = new Image();
-        img.onload = function() {
-          Meteor.call('eboypix.insert', img.src, img.width, img.height);
-          // setTimeout(function() {
-          nextImage(urls);
-          // }, 2000);
-        };
-        img.src = urls.shift();
-      }
-    })(urls.slice());
-  },
   'click .deleteAllDocs'() {
     Meteor.call('eboypix.deleteAll');
   },
---
+++ imports/ui/components/pixInput.js
@@ -0,0 +1,36 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+Template.pixInput.events({
+  'submit .new-pic-batch'(event) {
+    event.preventDefault();
+    // Assign form input to constant
+    const target = event.target;
+    const imgBatchURLs = target.batchchurls.value;
+    // Remove line breaks
+    const imgBatchURLsClean = imgBatchURLs.replace(/[\r\n]/g, ',');
+    console.log('imgBatchURLsClean: ' + imgBatchURLsClean);
+
+    // Create an array and populate with urls
+    let urls = [];
+    urls = imgBatchURLsClean.split(",");
+
+    // Only keep elements that start with 'http'
+    urls = urls.filter(function(element) {
+      return element.startsWith('http');
+    });
+
+    (function nextImage(urls) {
+      if (urls.length) {
+        let img = new Image();
+        img.onload = function() {
+          Meteor.call('eboypix.insert', img.src, img.width, img.height);
+          // setTimeout(function() {
+          nextImage(urls);
+          // }, 2000);
+        };
+        img.src = urls.shift();
+      }
+    })(urls.slice());
+  }
+});

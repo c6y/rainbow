fc796bf753fc4c1e1f97edd7335d609d1198bfd7
c6y <k@eboy.com>
2016-10-30 20:45:05 -0700

    cleanups

--- imports/ui/components/picOnList.js
+++ imports/ui/components/picOnList.js
@@ -1,8 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-import { Colors } from '../../api/colors/colors.js';
-
 import './picOnList.html';
 
 // Components used
@@ -17,41 +15,6 @@ import './edit/editCopyright.html';
 import './edit/editBackColor.js';
 import './edit/editBackColor.html';
 
-Template.picOnList.helpers({
-  colorHSL() {
-    // Get the color by name
-    const color = Colors.findOne(
-      { name: this.backgroundColor }
-    );
-    // If it's in the color database return hsl values as css hsl string
-    if (color) {
-      const hslColor = String(
-        'hsl(' +
-        color.hue + ', ' +
-        color.saturation + '%, ' +
-        color.luminosity + '%)'
-      );
-      return {
-        info: hslColor,
-        value: hslColor
-      };
-    }
-    // If color does not exist return diagonal stripes warning pattern
-    const emptyColor = 'repeating-linear-gradient(' +
-      '135deg,' +
-      'transparent,' +
-      'transparent 0.5rem,' +
-      '#ccc 0.5rem,' +
-      '#ccc 1rem' +
-      ')';
-    return {
-      info: 'Warning! Assign a color!',
-      value: emptyColor
-    };
-  }
-});
-
-// Template events
 Template.picOnList.events({
   'click .deletePic'() {
     Meteor.call('eboypix.delete', this._id);

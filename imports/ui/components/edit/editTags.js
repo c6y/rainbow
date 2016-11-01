2673cddeee7b176d48960b0ec2dbfce9bde08b95
c6y <k@eboy.com>
2016-11-01 01:28:25 -0700

    textarea to edit/add tags, remove duplicate tags

--- imports/ui/components/edit/editTags.js
+++ imports/ui/components/edit/editTags.js
@@ -8,13 +8,6 @@ Template.editTags.helpers({
     const tagsString = String(this.tags);
     const tagsStringSpaced = tagsString.replace(/,/g, ', ');
     return tagsStringSpaced;
-  },
-  initialInputWidth() {
-    // Set initial width of input field based on length of tags
-    const tagsString = String(this.tags);
-    const tagsStringSpaced = tagsString.replace(/,/g, ', ');
-    const lengthString = Math.max(tagsStringSpaced.length, 6);
-    return lengthString;
   }
 });
 
@@ -27,10 +20,7 @@ Template.editTags.events({
     // for nice display, add a space after commas
     const tagsStringSpaced = tagsStringClean.replace(/,/g, ', ');
     // live update input value
-    event.target.value = tagsStringSpaced;// http://www.w3schools.com/jsref/met_element_setattribute.asp
-    // Update width of input field as you type
-    const lengthString = Math.max(tagsStringSpaced.length, 6);
-    event.target.setAttribute('size', lengthString);
+    event.target.value = tagsStringSpaced;
   },
   'blur .editTags'(event, target) {
     // Restore tags input to original value
@@ -39,9 +29,6 @@ Template.editTags.events({
     // for nice display, add a space after commas
     const tagsStringSpaced = tagsString.replace(/,/g, ', ');
     event.target.value = tagsStringSpaced;
-    const originalLength = tagsStringSpaced.length;
-    // Restore width of input field
-    event.target.setAttribute('size', Math.max(originalLength, 6));
   },
   'keyup .editTags'(event, target) {
     if (event.keyCode === 13) { // return key submits new tags
@@ -50,9 +37,15 @@ Template.editTags.events({
       let tagsStringClean = tagsString.replace(/[^a-z0-9,-]/gi, '');
       // Remove initial and trailing commas
       tagsStringClean = tagsStringClean.replace(/(,*$)|(^,)/gi, '');
+      // Remove repeating commas
+      tagsStringClean = tagsStringClean.replace(/(, *,)/g, ',');
       // Turn string into a tags array
       const tagsArray = tagsStringClean.split(',');
-      Meteor.call('eboypix.updateTags', this._id, tagsArray);
+      // Remove duplicates from array
+      // (each value in a Set has to be unique)
+      const uniqueTagsArray = Array.from(new Set(tagsArray));
+
+      Meteor.call('eboypix.updateTags', this._id, uniqueTagsArray);
       // Deselect input field
       event.target.blur();
     } else if (event.keyCode === 27) { // escape restores tags

dffe627b78e0fb6c19fa9b5131e6cf0b9c779554
c6y <k@eboy.com>
2016-11-03 18:32:45 -0700

    Edit docs from last upload

--- imports/ui/components/pixPoolLatest.js
+++ imports/ui/components/pixPoolLatest.js
@@ -4,8 +4,8 @@ import { Session } from 'meteor/session';
 import { EboyPix } from '../../api/eboypix/eboypix.js';
 
 // Components used
-import './pic.html';
-import './pic.js';
+import './pixList.html';
+import './pixList.js';
 
 // Template helpers
 Template.pixPoolLatest.helpers({

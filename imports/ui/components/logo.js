b1b744818189ef3d13f6aa1805e6beb1e9dcc052
c6y <k@eboy.com>
2016-11-16 01:22:11 -0800

    logo back to home enabled if there's a query

--- imports/ui/components/logo.js
+++ imports/ui/components/logo.js
@@ -5,11 +5,15 @@ import './logo.html';
 
 Template.logo.helpers({
   toHomePath() {
-    // Return path to home, if not on pool, page 1
     const isPool = FlowRouter.getRouteName() === 'pool';
     const isPageOne = FlowRouter.getParam('page') === '1';
     const isSlugEverything = FlowRouter.getParam('slug') === 'everything';
-    if (isPool && isPageOne && isSlugEverything) {
+    const query = FlowRouter.getQueryParam('q');
+    let hasNoQuery = false;
+    if (query === undefined) {
+      hasNoQuery = true;
+    }
+    if (isPool && isPageOne && isSlugEverything && hasNoQuery) {
       return false;
     }
     return FlowRouter.path('pool', { slug: 'everything', page: 1 });

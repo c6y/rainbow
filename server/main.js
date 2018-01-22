c108dd4b1be3b62fa941dda53831c44b65ce462b
c6y <k@eboy.com>
2018-01-22 15:42:00 -0800

    Removed unnecessary Access-Control-Allow-Origin Header

--- server/main.js
+++ server/main.js
@@ -1,8 +1,2 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
-import { WebApp } from 'meteor/webapp';
-
-WebApp.rawConnectHandlers.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  return next();
-});

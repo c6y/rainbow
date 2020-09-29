36f131a07025a755319a60026039016f1f63b634
Kai Vermehr <k@eboy.com>
2020-09-28 19:10:08 -0700

    Live preview of css pattern input

--- imports/ui/components/cssbacks/cssBackInput.js
+++ imports/ui/components/cssbacks/cssBackInput.js
@@ -1,9 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { _ } from 'meteor/underscore';
+import { Session } from 'meteor/session';
 
 import './cssBackInput.html';
 
+// Template onCreated
+Template.cssBackInput.onCreated(function() {
+  const defaultColor = Meteor.settings.public.colors.debug;
+  Session.set('backgroundCss', 'background-color: ' + defaultColor + ';');
+});
+
 // Template helpers
+Template.cssBackInput.helpers({
+  cssPreview() {
+    return Session.get('backgroundCss');
+  },
+});
+
+// Template events
 Template.cssBackInput.events({
   'submit .new-cssBack'(event) {
     event.preventDefault();
@@ -16,4 +31,13 @@ Template.cssBackInput.events({
     target.name.value = '';
     target.code.value = '';
   },
+  'keyup textarea': _.throttle(function(event, target) {
+    if (event.target.value) {
+      console.log('event.target.value: ' + event.target.value);
+      Session.set('backgroundCss', event.target.value);
+    } else {
+      const defaultColor = Meteor.settings.public.colors.debug;
+      Session.set('backgroundCss', 'background-color: ' + defaultColor + ';');
+    }
+  }, 600),
 });
